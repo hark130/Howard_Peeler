12eafd92f5fc94fa5691dc6637157ad18fd89d42
Joseph Harkleroad <hark130@yahoo.com>
2020-04-12 01:13:29 -0500

    3-1: Implemented new espeak functionality
    This is really just a temp holder but it has been functionally tested
    
    Changes to be committed:
    	modified:   src/HOPE/__main__.py

--- src/HOPE/__main__.py
+++ src/HOPE/__main__.py
@@ -3,6 +3,7 @@
 # Local Imports
 from HOPE_Logger import log_a_string, Log_Cats
 from HOPE_Obfuscation import obfuscate_string
+from HOPE_Scream import verify_espeak, Espeak_Config
 from HOPE_Subprocess import execute_command
 
 
@@ -14,16 +15,20 @@ def main():
     mask = 'Howard_Peeler'
     obfuscated_URL = ' \x1b\x03\x11\x01^p\x7f\x12\x12\x1bK\x1a-\rY\x02\x1d\tp'
     clear_text_URL = ''
+    espeak_obj = None
+    espeak_cmd_list = []
 
     # TEST
-    try:
-        log_a_string(execute_command(['espeak', '--version']), Log_Cats.DEBUG)
-    except RuntimeError as err:
-        log_a_string(repr(err), Log_Cats.DEBUG)
+    # espeak
+    verify_espeak()
 
     # DEOBFUSCATE URL
     clear_text_URL = obfuscate_string(obfuscated_URL, mask)
     log_a_string('De-obfuscated: ' + clear_text_URL, Log_Cats.INFO)
+
+    # ESPEAK
+    espeak_obj = Espeak_Config()
+    espeak_obj.speak('This is a TEST')
 
     # DONE
     log_a_string('HOPE Stop', Log_Cats.INFO)
