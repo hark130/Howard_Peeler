171cc4fb23d749ada1d2e40492a6c2bca4b53923
Joseph Harkleroad <hark130@yahoo.com>
2020-04-12 21:55:43 -0500

    3-4: REfactored HOPE_Scream.py
    class Espeak_Config
    	- create_command_list() is now private
    	- speak() updated to use new private method
    
    class HOPE_Scream
    	- Initial Commit (and mostly functional)
    	- Wrapper around other methods of notification
    	- Attempts: 1. espeak, 2. tput, 3. DEBUG log message
    
    verify_tput()
    	- Initial Commit (and functional)
    
    NOTE: There is a shunt in verify_espeak() to enable functional testing
    of tput functionality.
    
    Changes to be committed:
    	modified:   src/HOPE/HOPE_Scream.py

--- src/HOPE/HOPE_Scream.py
+++ src/HOPE/HOPE_Scream.py
@@ -1,3 +1,4 @@
+import subprocess
 from HOPE_Logger import log_a_string, Log_Cats
 from HOPE_Subprocess import execute_command
 
@@ -21,7 +22,123 @@ def verify_espeak():
         log_a_string(version_output, Log_Cats.DEBUG)
 
     # DONE
+    return False  # FUNCTIONAL TESTING
     return installed
+
+
+def verify_tput():
+    """
+        PURPOSE - Verify tput is installed
+        RETURNS True if tput is installed
+    """
+    # LOCAL VARIABLES
+    installed = True
+    command_output = ''
+
+    # CHECK IT
+    try:
+        command_output = execute_command(['tput', '-V'])
+    except Exception:
+        installed = False
+        log_a_string('tput is not installed', Log_Cats.ERR)
+    else:
+        log_a_string(command_output, Log_Cats.DEBUG)
+
+    # DONE
+    # return False  # FUNCTIONAL TESTING
+    return installed
+
+
+"""
+    PURPOSE - Make a sound.  Hopefully, speech.  Otherwise, some beeps.
+    NOTES
+        - This class implements the following methods, in order, to alert the caller:
+            1. HOPE_Scream.speak(message) --> espeak message
+            2. HOPE_Scream.speak(message) --> tput bel
+            3. HOPE_Scream.speak(message) --> [DEBUG] message
+"""
+class HOPE_Scream:
+
+    def __init__(self):
+        # ATTRIBUTES
+        # self._espeak_installed
+        self._espeak_installed = verify_espeak()
+        # self._espeak_config_obj
+        if self._espeak_installed:
+            self._espeak_config_obj = Espeak_Config()
+        else:
+            self._espeak_config_obj = None
+        # self._tput_installed
+        self._tput_installed = verify_tput()
+
+    def speak(self, words):
+        # INPUT VALIDATION
+        self._validate_attributes(words)
+
+        # LEARN TO SPEAK
+        if self._espeak_installed:
+            self._espeak_config_obj.speak(words)
+        elif self._tput_installed:
+            self._tput_it(words)
+        else:
+            log_a_string(words, Log_Cats.NOTICE)
+
+    def _validate_attributes(self, words):
+        # INPUT VALIDATION
+        # self._words
+        self._validate_words(words)
+
+        # self._espeak_installed
+        if not isinstance(self._espeak_installed, bool):
+            raise TypeError('The _espeak_installed must be a bool instead of '
+                            f'{type(self._espeak_installed)}')
+
+        # self._espeak_config_obj
+        if self._espeak_installed and not self._espeak_config_obj:
+            raise RuntimeError('Espeak is installed but there is no Espeak_Config object')
+
+        # self._tput_installed
+        if not isinstance(self._tput_installed, bool):
+            raise TypeError('The _tput_installed must be a bool instead of '
+                            f'{type(self._tput_installed)}')
+
+    def _validate_words(self, words):
+        # VALIDATION
+        if not isinstance(words, str):
+            raise TypeError('The words parameter must be a string instead of '
+                            f'{type(words)}')
+        elif not words:
+            raise ValueError('The words parameter may not be empty')
+
+    def _tput_it(self, words):
+        """
+            PURPOSE - Converts a string into system beeps.  One beep per word.
+            PARAMETERS
+                words - Non-empty string to convert into beeps
+            NOTES
+                - Implements "tput bel" under the hood
+        """
+        # LOCAL VARIABLES
+        default_command_list = ['tput']
+        bell_string = 'bel'
+        command_list = []
+
+        # CONSTRUCT COMMAND LIST
+        # Count words
+
+        # Build command list
+        command_list = default_command_list + [bell_string]
+
+        # EXECUTE COMMAND LIST
+        # execute_command(command_list)
+        log_a_string(f'HOPE_Scream is executing {repr(command_list)}', Log_Cats.DEBUG)  # DEBUGGING
+        # subprocess.run(command_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # DEBUGGING
+        # subprocess.run(command_list, stderr=subprocess.PIPE)  # DEBUGGING
+        subprocess.run(command_list)  # DEBUGGING
+        # subprocess.run(['sleep', '1'])  # DEBUGGING
+        # subprocess.run(command_list)  # DEBUGGING
+        # subprocess.run(command_list, stderr=subprocess.DEVNULL)  # DEBUGGING
+        # subprocess.run(command_list, shell=True)  # DEBUGGING
 
 
 class Espeak_Config:
@@ -36,6 +153,19 @@ class Espeak_Config:
         self._pitch = pitch
         self._wpms = wpms
         self._string_delims = ["'", '"']
+
+    def speak(self, words):
+        # VALIDATION
+        # Attributes
+        self._validate_attributes()
+        # words
+        self._validate_words(words)
+
+        # LOCAL VARIABLES
+        command_list = self._create_command_list(words)
+
+        # EXECUTE COMMAND LIST
+        execute_command(command_list)
 
     def _validate_attributes(self):
         """
@@ -69,20 +199,7 @@ class Espeak_Config:
         elif not words:
             raise ValueError('The words parameter may not be empty')
 
-    def speak(self, words):
-        # VALIDATION
-        # Attributes
-        self._validate_attributes()
-        # words
-        self._validate_words(words)
-
-        # LOCAL VARIABLES
-        command_list = self.create_command_list(words)
-
-        # EXECUTE COMMAND LIST
-        execute_command(command_list)
-
-    def create_command_list(self, words):
+    def _create_command_list(self, words):
         """
             PURPOSE - Builds the espeak command list based on attributes
             RETURNS a list of strings
